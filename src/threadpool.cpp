8e0c3bf75d26123f9861055402305180df1e9882
Eric <hayter.eric@gmail.com>
2025-05-11 10:59:21 -0400

    Create rough draft of threadpool implementation
    implemented a simple threadpool going to do some manual testing to see nothing breaks (hopefully)

--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -1,1 +1,65 @@
 #include "threadpool.h"
+#include <algorithm>
+#include <mutex>
+
+void ThreadPool::add_task(const std::function<void ()> &task)
+{
+	{
+		std::lock_guard<std::mutex> lk(m_mut);
+		m_tasks.push(task);
+	}
+	m_cv.notify_one();
+}
+
+void ThreadPool::join()
+{
+	std::unique_lock<std::mutex> lk(m_mut);
+	m_cv.wait(lk, [this](){ m_tasks.empty(); });
+}
+
+ThreadPool::ThreadPool() : ThreadPool(std::max(static_cast<int>(std::thread::hardware_concurrency()) - 1, 1)) {}
+
+ThreadPool::ThreadPool(int thread_count) : m_thread_count(thread_count)
+{
+	m_workers.reserve(m_thread_count);
+	auto worker_function = [this](){
+		std::unique_lock<std::mutex> lk(m_mut);
+		while (true)
+		{
+			m_cv.wait(lk, [this](){ return m_terminate || not m_tasks.empty(); });
+			// ready for cleanup with no more tasks
+			if (m_tasks.empty() && m_terminate)
+				return;
+
+			// get task
+			auto task = m_tasks.front();
+			m_tasks.pop();
+			lk.unlock();
+
+			task();
+		};
+	};
+
+	for (int i = 0; i < m_thread_count; i++)
+	{
+		m_workers.push_back(std::thread(worker_function));
+	}
+}
+
+
+ThreadPool::~ThreadPool()
+{
+	{
+		// notify the worker threads to stop
+		std::lock_guard<std::mutex> lk(m_mut);
+		m_terminate = true;
+	}
+	m_cv.notify_all();
+
+	for (std::thread& worker: m_workers)
+	{
+		if (worker.joinable())
+			worker.join();
+	}
+}
+
--- src/threadpool.h
+++ src/threadpool.h
@@ -5,15 +5,33 @@
 #include <functional>
 #include <thread>
 #include <condition_variable>
+#include <algorithm>
 
 class ThreadPool
 {
-	private:
-	std::queue<std::function<void ()>> pool;
-	std::mutex mut;
-	std::condition_variable cv;
-	
 	public:
-	void await_add_task(const std::function<void ()> &task);
+	void add_task(const std::function<void ()> &task);
 	void join();
-}
+	int thread_count() { return m_thread_count; };
+
+	ThreadPool();
+	ThreadPool(int thread_count);
+
+	// copy constructors are not allowed.
+	ThreadPool(const ThreadPool &other) = delete;
+	ThreadPool operator=(const ThreadPool &other) = delete;
+
+	// we do allow moves if desired.
+	ThreadPool(const ThreadPool &&other) noexcept;
+	ThreadPool& operator=(const ThreadPool &&other) noexcept;
+
+	~ThreadPool();
+
+	private:
+	std::vector<std::thread> m_workers;
+	std::queue<std::function<void ()>> m_tasks;
+	std::mutex m_mut;
+	std::condition_variable m_cv;
+	int m_thread_count;
+	bool m_terminate = false;
+};
